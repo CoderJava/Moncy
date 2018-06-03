93d3360d6379f181c4e780ddddf4a0e7eeb5fa7a
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-06-03 10:55:15 +0700

    Create SplashScreenActivity.kt and activity_splash_screen.xml file

---
+++ app/src/main/java/com/ysn/moncy/views/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.ysn.moncy.views.ui.splashscreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ysn.moncy.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
