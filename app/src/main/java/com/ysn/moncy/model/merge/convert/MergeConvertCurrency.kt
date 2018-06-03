44783f662e971b91ef9ea5eac05e05e7eda93e95
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-06-03 10:50:52 +0700

    Reset project

--- app/src/main/java/com/ysn/moncy/model/merge/convert/MergeConvertCurrency.kt
+++
@@ -1,9 +0,0 @@
-package com.ysn.moncy.model.merge.convert
-
-/**
- * Created by root on 26/08/17.
- */
-data class MergeConvertCurrency (
-        var currencyCode: String,
-        var flag: String
-)
