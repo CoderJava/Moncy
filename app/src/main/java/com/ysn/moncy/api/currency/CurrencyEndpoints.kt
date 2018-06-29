b046e9ba6299807991c6dfda8efaa8b0ca6838b2
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-06-29 08:14:56 +0700

    Create CurrencyEndpoints.kt file

--- app/src/main/java/com/ysn/moncy/api/currency/CurrencyEndpoints.kt
+++ app/src/main/java/com/ysn/moncy/api/currency/CurrencyEndpoints.kt
@@ -1,4 +1,15 @@
 package com.ysn.moncy.api.currency
 
+import com.ysn.moncy.BuildConfig
+import com.ysn.moncy.model.currencynow.CurrencyNow
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface CurrencyEndpoints {
+
+    /* http://apilayer.net/api/live?access_key=257cb471003f7806c5706d137f67a338 */
+    @GET("live")
+    fun getLiveCurrency(@Query("access_key") accessKey: String = BuildConfig.API_KEY_CURRENCY): Observable<CurrencyNow>
+
 }
