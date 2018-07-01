c3cfb272227de099fbfdeb912c874d74b017d17d
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-01 22:04:04 +0700

    Add endpoint getCountries in CountryEndpoints.kt file

--- app/src/main/java/com/ysn/moncy/api/country/CountryEndpoints.kt
+++ app/src/main/java/com/ysn/moncy/api/country/CountryEndpoints.kt
@@ -1,4 +1,11 @@
 package com.ysn.moncy.api.country
 
+import com.ysn.moncy.model.country.CountryData
+import io.reactivex.Observable
+import retrofit2.http.GET
+
 interface CountryEndpoints {
+
+    @GET("rest/v2")
+    fun getCountries(): Observable<List<CountryData>>
 }
