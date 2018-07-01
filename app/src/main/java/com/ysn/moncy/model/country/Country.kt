44608608464990c1b10b41d9dddac9939b432240
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-01 22:09:10 +0700

    Create data class Country.kt file

---
+++ app/src/main/java/com/ysn/moncy/model/country/Country.kt
@@ -0,0 +1,8 @@
+package com.ysn.moncy.model.country
+
+data class Country(
+        val flag: String,
+        val name: String,
+        val symbolCurrency: String,
+        val capitalCity: String
+)
