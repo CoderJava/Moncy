b02adaebbdab418a8ee1c0dc9c6fab3bfbbc86fc
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-01 11:38:03 +0700

    Create CountryActivity.kt and activity_country.xml file

---
+++ app/src/main/java/com/ysn/moncy/views/ui/activity/country/CountryActivity.kt
@@ -0,0 +1,13 @@
+package com.ysn.moncy.views.ui.activity.country
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ysn.moncy.R
+
+class CountryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country)
+    }
+}
