d509c34ddce1ce65c5c2b74cc4e8d1b369d37e4a
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-01 22:12:32 +0700

    Create function load data for CountryActivity

--- app/src/main/java/com/ysn/moncy/views/ui/activity/country/CountryPresenter.kt
+++ app/src/main/java/com/ysn/moncy/views/ui/activity/country/CountryPresenter.kt
@@ -1,10 +1,61 @@
 package com.ysn.moncy.views.ui.activity.country
 
+import com.ysn.moncy.api.country.CountryEndpoints
+import com.ysn.moncy.api.countrycurrency.CountryCurrencyEndpoints
+import com.ysn.moncy.model.country.Country
+import com.ysn.moncy.model.country.CountryData
 import com.ysn.moncy.views.base.mvp.BasePresenter
-import com.ysn.moncy.views.base.mvp.Presenter
+import com.ysn.moncy.views.ui.activity.country.adapter.AdapterCountry
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
 
-class CountryPresenter : BasePresenter<CountryView>() {
+class CountryPresenter constructor(private val countryEndpoints: CountryEndpoints,
+                                   private val countryCurrencyEndpoints: CountryCurrencyEndpoints) : BasePresenter<CountryView>() {
 
+    private lateinit var adapterCountry: AdapterCountry
+    private var countries = mutableListOf<Country>()
+
+    fun onLoadData() {
+        val context = view?.getViewContext()
+        adapterCountry = AdapterCountry(context = context!!, countries = countries)
+        val observableCountry = countryEndpoints.getCountries()
+        val observableCountryCurrency = countryCurrencyEndpoints.getCurrencyCode()
+        Observable
+                .zip(observableCountry, observableCountryCurrency, BiFunction<List<CountryData>, Map<String, String>, Boolean> { countryDatas, mapCountryCurrency ->
+                    mapCountryCurrency.forEach { key, value ->
+                        for (index in countryDatas.indices) {
+                            val countryData = countryDatas[index]
+                            if (countryData.currencies[0].code == value) {
+                                val countryName = countryData.name
+                                val symbolCurrency = value
+                                val capitalCity = countryData.capital
+                                val flagCountry = countryData.flag
+                                val country = Country(flag = flagCountry, name = countryName, symbolCurrency = symbolCurrency, capitalCity = capitalCity)
+                                countries.add(country)
+                                break
+                            }
+                        }
+                    }
+                    true
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            adapterCountry.refresh(countries = countries)
+                            view?.loadData(adapterCountry = adapterCountry)
+                        },
+                        {
+                            it.printStackTrace()
+                            view?.loadDataFailed(message = it.message)
+                        },
+                        {
+                            /* nothing to do in here */
+                        }
+                )
+    }
 
 
 }
--- app/src/main/java/com/ysn/moncy/views/ui/activity/country/CountryView.kt
+++ app/src/main/java/com/ysn/moncy/views/ui/activity/country/CountryView.kt
@@ -1,7 +1,15 @@
 package com.ysn.moncy.views.ui.activity.country
 
+import android.content.Context
 import com.ysn.moncy.views.base.mvp.BaseView
+import com.ysn.moncy.views.ui.activity.country.adapter.AdapterCountry
 
 interface CountryView : BaseView {
+
+    fun getViewContext(): Context
+
+    fun loadDataFailed(message: String?)
+
+    fun loadData(adapterCountry: AdapterCountry)
 
 }
