86da7f56e9773d093070692a4f6b8c992dc3f26c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-01 22:10:41 +0700

    Add function provideCountryPresenter in CountryActivityModule.kt file

--- app/src/main/java/com/ysn/moncy/di/module/activity/country/CountryActivityModule.kt
+++ app/src/main/java/com/ysn/moncy/di/module/activity/country/CountryActivityModule.kt
@@ -1,5 +1,7 @@
 package com.ysn.moncy.di.module.activity.country
 
+import com.ysn.moncy.api.country.CountryEndpoints
+import com.ysn.moncy.api.countrycurrency.CountryCurrencyEndpoints
 import com.ysn.moncy.di.ActivityScope
 import com.ysn.moncy.views.ui.activity.country.CountryPresenter
 import dagger.Module
@@ -10,6 +12,7 @@ class CountryActivityModule {
 
     @ActivityScope
     @Provides
-    internal fun provideCountryPresenter(): CountryPresenter = CountryPresenter()
+    internal fun provideCountryPresenter(countryEndpoints: CountryEndpoints, countryCurrencyEndpoints: CountryCurrencyEndpoints):
+            CountryPresenter = CountryPresenter(countryEndpoints = countryEndpoints, countryCurrencyEndpoints = countryCurrencyEndpoints)
 
 }
