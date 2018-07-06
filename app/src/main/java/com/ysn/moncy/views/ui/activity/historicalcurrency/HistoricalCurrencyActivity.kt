ff2bc759c60e88daf000a1f8aff7b0204157b29b
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-07-06 07:45:09 +0700

    Create HistoricalCurrencyActivity.kt and activity_historical_currency.xml file

---
+++ app/src/main/java/com/ysn/moncy/views/ui/activity/historicalcurrency/HistoricalCurrencyActivity.kt
@@ -0,0 +1,13 @@
+package com.ysn.moncy.views.ui.activity.historicalcurrency
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ysn.moncy.R
+
+class HistoricalCurrencyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historical_currency)
+    }
+}
